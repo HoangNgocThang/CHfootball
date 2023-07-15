a90fd267c45cc2f69ea9e8e0696b0a40d0851977
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-15 11:57:45 +0700

    test react-bootstrap

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -4,9 +4,25 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Detail() {
-    return <div>
-        <p>App Detail</p>
-    </div>
+    return (<Container>
+        <Row>
+            <Col>1 of 1</Col>
+
+            <Col>1 of 1</Col>
+
+            <Col
+                sm={12}
+                className='d-lg-none d-md-none d-xl-none d-xxl-none'
+                style={{backgroundColor: 'red'}}>
+                <Row>
+                    <span>12 of mobile</span>.
+                </Row>
+                <Row>
+                    <span>12 of mobile</span>.
+                </Row>
+            </Col>
+        </Row>
+    </Container>)
 }
 
 export default Detail;
