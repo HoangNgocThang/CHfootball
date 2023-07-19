c5d4cf23c456c6053ab27db757b46d5c1a606141
hoangngocthang <>
2023-07-19 21:43:38 +0700

    cm

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,14 +16,6 @@ import phukien from '../images/phukien.png';
 import TCarousel from "../components/TCarousel";
 import TButtonOrder from "../components/TButtonOrder";
 
-// const items = [{
-//     src: 'https://picsum.photos/id/123/1200/400', altText: 'CHFOOTBALL', caption: 'CHFOOTBALL', key: 1,
-// }, {
-//     src: 'https://picsum.photos/id/456/1200/400', altText: 'CHFOOTBALL', caption: 'Slide 2', key: 2,
-// }, {
-//     src: 'https://picsum.photos/id/678/1200/400', altText: 'CHFOOTBALL', caption: 'Slide 3', key: 3,
-// }];
-
 function Home() {
     const windowWidth = useRef(window.innerWidth);
     const windowHeight = useRef(window.innerHeight);
