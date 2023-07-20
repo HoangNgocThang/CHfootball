2c5e9453726295f01f2e11e31cf60010450c982a
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-20 14:05:03 +0700

    container customer

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,13 +15,29 @@ import mauthietke from '../images/mauthietke.png';
 import phukien from '../images/phukien.png';
 import TCarousel from "../components/TCarousel";
 import TButtonOrder from "../components/TButtonOrder";
-import bosch from '../images/bosch.png';
+import shb from '../images/shb.jpg';
 import bothongtin from '../images/bothongtin.png';
 import congdoan from '../images/congdoan.png';
+import bosch from '../images/bosch2.png';
+import vnpdt from '../images/vnpdt.jpg';
 import coopbank from '../images/coopbank.png';
 import pv from '../images/pv.png';
-import shb from '../images/shb.jpg';
-import vnpdt from '../images/vnpdt.jpg';
+import buuchinhvienthong from '../images/buuchinhvienthong.jpg';
+
+import c1 from '../images/c1.jpg';
+import c2 from '../images/c2.jpg';
+import c3 from '../images/c3.png';
+import c4 from '../images/c4.jpg';
+import c5 from '../images/c5.jpg';
+import c6 from '../images/c6.jpg';
+import c7 from '../images/c7.jpg';
+import c8 from '../images/c8.jpg';
+import c9 from '../images/c9.jpg';
+import c10 from '../images/c10.jpg';
+import c11 from '../images/c11.jpg';
+import c12 from '../images/c12.jpg';
+import c13 from '../images/c13.jpg';
+import c14 from '../images/c14.jpg';
 
 function Home() {
     const windowWidth = useRef(window.innerWidth);
@@ -444,136 +460,67 @@ Các phần quà đặc biệt cho khách hàng quay lại/ khách hàng thân t
         {/*Desktop product type --- end*/}
 
         {/*Mobile customer ---start*/}
-        <div className={"d-flex container-product-type"}>
+        <div className={"container-customer"}>
             <h3 style={{fontWeight: 'bold'}}>{`Khách hàng của CHFootball`}</h3>
+            <div>
+                <div style={{width: 100, height: 1, backgroundColor: 'black'}}></div>
+            </div>
+            <div className={"container-customer-wrap-imgs"}>
+                <div style={{width: 35, height: 35}}>
+                    <img src={shb} style={{width: 35, height: 35, backgroundColor: 'white'}} alt={"shb"}/>
+                </div>
+                <div style={{width: 35, height: 35}}>
+                    <img src={bothongtin} style={{width: 35, height: 35, backgroundColor: 'white'}} alt={"bothongtin"}/>
+                </div>
+                <div style={{width: 35, height: 35}}>
+                    <img src={bosch} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+                <div style={{width: 35, height: 35}}>
+                    <img src={congdoan} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+                <div style={{width: 30, height: 30}}>
+                    <img src={buuchinhvienthong} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+                <div style={{width: 35, height: 35}}>
+                    <img src={vnpdt} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+                <div style={{width: 35, height: 35}}>
+                    <img src={coopbank} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+                <div style={{width: 30, height: 30}}>
+                    <img src={pv} style={{width: 35, height: 35, backgroundColor: 'white'}}/>
+                </div>
+            </div>
 
-            <div style={{
-                display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'
-            }}>
-                <div style={{width: 150, height: 1, backgroundColor: 'black'}}></div>
+            <div className="container-products">
+                <div className='iteamcard' >
+                    <img src={c6} className={"container-customer-img"}/>
+                    <img src={c1} className={"container-customer-img"}/>
+                    <img src={c2} className={"container-customer-img"}/>
+                </div>
+                <div className='iteamcard'>
+                    <img src={c3} className={"container-customer-img"}/>
+                    <img src={c4} className={"container-customer-img"}/>
+                    <img src={c5} className={"container-customer-img"}/>
+                    <img src={c14} className={"container-customer-img"}/>
+                </div>
+                <div className='iteamcard'>
+                    <img src={c7} className={"container-customer-img"}/>
+                    <img src={c8} className={"container-customer-img"}/>
+                    <img src={c7} className={"container-customer-img"}/>
+                    <img src={c13} className={"container-customer-img"}/>
+                </div>
+                <div className='iteamcard'>
+                    <img src={c9} className={"container-customer-img"}/>
+                    <img src={c10} className={"container-customer-img"}/>
+                    <img src={c11} className={"container-customer-img"}/>
+                    <img src={c12} className={"container-customer-img"}/>
+                </div>
             </div>
         </div>
         {/*Mobile customer ---end*/}
 
-        {/*<div className="container-products">*/}
-        {/*    {data.map((e, i) => {*/}
-        {/*        if (i === 0) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src="https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/342972352_747609960370931_4792094069670198249_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cFH3RlTRPMEAX_Csl_c&_nc_ht=scontent.fhan3-3.fna&oh=00_AfD2OutI2WpFWf0Kqe_CYqpl39mp3C3ilrLP4OWU0IzRIQ&oe=64A93409"*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/301953345_126103930171998_7526762796660612478_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=wXxJ3DgAyVMAX89mXua&_nc_ht=scontent.fhan3-2.fna&oh=00_AfCed9CmMAHYZgD6fEfF6J00EomLM_uXNqwxrIQRbDbSRQ&oe=64AA3752"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/307703674_129639756485082_3073392131615707292_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=NIlpQ88bx2IAX8cdmEw&_nc_ht=scontent.fhan3-3.fna&oh=00_AfBF5AGBE_XaH9IC5c1TJ9v4C_wPkRJAWD2Guqti90H0Ng&oe=64A99904"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        if (i === 1) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src="https://scontent.fhan4-1.fna.fbcdn.net/v/t39.30808-6/306355429_129393236509734_7510365682179228330_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=kkkVXgxXot4AX-7_wqc&_nc_ht=scontent.fhan4-1.fna&oh=00_AfCCvLKkTeRPT_erRg24oW9q1JwfDtYc46cMqtBafL_eNg&oe=64A9FDB7"*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/306000102_127792343336490_8860810274989650270_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=eScOzB8skOwAX-EeiPh&_nc_ht=scontent.fhan3-4.fna&oh=00_AfA5JoEELj-4b_zTMdvS7TJ6y6AvPCiDIiNURD2itbO3WA&oe=64AA1175"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/306086195_127385943377130_4820730886942250421_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=YpFpePIGmwwAX_A5Kb8&_nc_ht=scontent.fhan3-4.fna&oh=00_AfAEmdUHEEUhY9MyTeLDuT0Yl0RAxBljs_EwdUH3HQ8T1w&oe=64AAA4CC"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        if (i === 2) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-2.fna.fbcdn.net/v/t39.30808-6/299111645_120607507388307_3699044204930310152_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0debeb&_nc_ohc=jEsDUerkpWsAX_biCKS&_nc_ht=scontent.fhan3-2.fna&oh=00_AfBFjSbvo4IT_QExoJ1eiexqh8r43Zmxbn4QaYLIbpbP2w&oe=64A9A7E2"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan4-3.fna.fbcdn.net/v/t39.30808-6/342982898_935689507745872_1801466322926761694_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_L0cvfHY484AX81sb2i&_nc_ht=scontent.fhan4-3.fna&oh=00_AfCPf3Jxxn8X4HmakZDkSnD10ipe_gB7iAnibckXwT61sA&oe=64AA5CAE"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan4-2.fna.fbcdn.net/v/t39.30808-6/306948536_129999626449095_62413514750212951_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=4g7C72RiVEUAX8L5Z1M&_nc_ht=scontent.fhan4-2.fna&oh=00_AfA556fLUeVitWjfsSeXxNsHaNdrcDTaI1JcmKK-3loxNA&oe=64AA0403"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        if (i === 3) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src="https://scontent.fhan3-1.fna.fbcdn.net/v/t39.30808-6/305979129_127792336669824_4878188787227361786_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fGqVMztiG8kAX8NVRxd&_nc_ht=scontent.fhan3-1.fna&oh=00_AfAZ1G0YFwZs-VAMgOhZpa8Ms1j9sQQpLS1nUSQIHSoHbQ&oe=64A9CB17"*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan4-1.fna.fbcdn.net/v/t39.30808-6/300969367_123442587104799_760124374553361034_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=0debeb&_nc_ohc=6ZdBNc8iomkAX_Ifmno&_nc_ht=scontent.fhan4-1.fna&oh=00_AfByNvThxU6LMxuBXm1mEULjP1KICnxwAKhVxyYMp4Cm5g&oe=64A93319"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://firebasestorage.googleapis.com/v0/b/tintucmoi24h-6c17a.appspot.com/o/347589927_2256513847854980_3155375348178503441_n.jpg?alt=media&token=bbea583b-90a6-4ad3-855d-a1537e883d57"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        if (i === 4) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src="https://firebasestorage.googleapis.com/v0/b/tintucmoi24h-6c17a.appspot.com/o/v.png?alt=media&token=d6b80be7-b2b0-4a6f-ae65-f4757c3172d8"*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/309224583_132556372860087_3199277604602885980_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=XFUeOLeSIp4AX_KRHtK&_nc_ht=scontent.fhan3-4.fna&oh=00_AfAnp6_ouPw0rvaqKZ1gSVGzvfirgV8lvsJA1bI7pcGnLQ&oe=64AAEF0C"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan4-2.fna.fbcdn.net/v/t39.30808-6/347621983_240535082062215_7841174145727718490_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ihrGPtyZ7LUAX91_Jxj&_nc_ht=scontent.fhan4-2.fna&oh=00_AfAm_zcF60k9JfDYT5evi9QxR93wVTmdtnnW3GNHkmG-TA&oe=64A9AFA1"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        if (i === 5) {*/}
-        {/*            return <div className='iteamcard' key={i + ""}>*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src="https://scontent.fhan4-3.fna.fbcdn.net/v/t39.30808-6/314958447_145141584934899_4660768754789138922_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=d_j14U3efYsAX8xnYAB&_nc_ht=scontent.fhan4-3.fna&oh=00_AfBWEV3gE4HOELufgbjEDOjZjllsXw3gDtcPF1avEkbUBA&oe=64AA7F76"*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-3.fna.fbcdn.net/v/t39.30808-6/315889205_146744751441249_9082638654701276282_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=rZpIq3ol4ugAX-mA4yk&_nc_ht=scontent.fhan3-3.fna&oh=00_AfBf0jmZHTIlu-yz9Itwkb7k_4tOUbHctC11N4n4bP04JQ&oe=64AB0C61"}*/}
-        {/*                />*/}
-        {/*                <img*/}
-        {/*                    className={'itemcard-img'}*/}
-        {/*                    alt="Sample"*/}
-        {/*                    src={"https://scontent.fhan3-1.fna.fbcdn.net/v/t39.30808-6/316414969_148190024630055_2555280542635369633_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=uhj7tD4eZaMAX_QVhRI&_nc_ht=scontent.fhan3-1.fna&oh=00_AfCj4etg21dY2uInC96BfzUcqoTeEj9lV-DLC7jpLngfJg&oe=64AA585D"}*/}
-        {/*                />*/}
-        {/*            </div>*/}
-        {/*        }*/}
-        {/*        return <></>*/}
-        {/*    })}*/}
-        {/*</div>*/}
+
 
     </div>);
 }
