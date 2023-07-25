600dff81c73fe50362086f7a6768cb9964b8c4c1
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-25 11:49:31 +0700

    cm

--- src/pages/home.js
+++ src/pages/home.js
@@ -793,11 +793,43 @@ Các phần quà đặc biệt cho khách hàng quay lại/ khách hàng thân t
         {/*Model Công nghệ may --- start*/}
         <div id="myModalMay" className="modal--thanghn">
             <div className="modal-content--thanghn">
-                <span className="close" onClick={() => {
+                <span style={{paddingRight: 5, paddingLeft: 5}}
+                      className="close" onClick={() => {
                     var modal = document.getElementById("myModalMay");
                     modal.style.display = "none";
                 }}>&times;</span>
-                <p>Some text in the Modal..</p>
+                <div style={{display: 'flex', flex: 1, gap: 10}}>
+                    <div style={{
+                        textAlign: 'left',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: "flex-start", // position: 'relative'
+                    }}>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: "rgb(5, 31, 77)", // position: 'absolute',
+                            // top: 10, left: 10,
+                            // padding: 20,
+                        }}>
+                            <p style={{fontSize: 30, color: 'white', margin: 0, padding: 0}}>CÔNG NGHỆ MAY</p>
+                        </div>
+                        <p style={{marginTop: 30}}>Dây chuyền sản xuất của CHF có nhiều loại vải phù hợp đặc thù khí hậu
+                            ở Việt Nam với
+                            chất liệu co dãn, thấm hút mồ hôi. Các loại vải phổ biến sử dụng nhiều như:</p>
+                        <div style={{display: 'flex', flexDirection: 'row'}}>
+                            <div style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                                <p>- Vải thun lạnh</p>
+                                <p>- Vải mè thái</p>
+                                <p>- Vải adidas vuông</p>
+                                <p>- Vải adidas mịn</p>
+                                <p>- Vải kim cương 4 chiều</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={detailtech1} style={{width: 400, height: 400}}/>
+                </div>
             </div>
         </div>
         {/*Model Công nghệ may --- end*/}
@@ -805,11 +837,43 @@ Các phần quà đặc biệt cho khách hàng quay lại/ khách hàng thân t
         {/*Model Công nghệ in thêu --- start*/}
         <div id="myModalInTheu" className="modal--thanghn">
             <div className="modal-content--thanghn">
-                <span className="close" onClick={() => {
+                <span style={{paddingRight: 5, paddingLeft: 5}}
+                      className="close" onClick={() => {
                     var modal = document.getElementById("myModalInTheu");
                     modal.style.display = "none";
                 }}>&times;</span>
-                <p>Some text in the Modal..</p>
+                <div style={{display: 'flex', flex: 1, gap: 10}}>
+                    <div style={{
+                        textAlign: 'left',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: "flex-start", // position: 'relative'
+                    }}>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: "rgb(5, 31, 77)", // position: 'absolute',
+                            // top: 10, left: 10,
+                            // padding: 20,
+                        }}>
+                            <p style={{fontSize: 30, color: 'white', margin: 0, padding: 0}}>CÔNG NGHỆ IN-THÊU</p>
+                        </div>
+                        <p style={{marginTop: 30}}>Dây chuyền sản xuất của CHF có nhiều loại vải phù hợp đặc thù khí hậu
+                            ở Việt Nam với
+                            chất liệu co dãn, thấm hút mồ hôi. Các loại vải phổ biến sử dụng nhiều như:</p>
+                        <div style={{display: 'flex', flexDirection: 'row'}}>
+                            <div style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                                <p>- Vải thun lạnh</p>
+                                <p>- Vải mè thái</p>
+                                <p>- Vải adidas vuông</p>
+                                <p>- Vải adidas mịn</p>
+                                <p>- Vải kim cương 4 chiều</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={detailtech1} style={{width: 400, height: 400}}/>
+                </div>
             </div>
         </div>
         {/*Model Công nghệ in thêu  --- end*/}
