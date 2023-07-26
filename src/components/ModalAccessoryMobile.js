fa1f97f583d606f84a2344b57d6ecdd17e0da60f
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-26 16:42:35 +0700

    cm

--- src/components/ModalAccessoryMobile.js
+++ src/components/ModalAccessoryMobile.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import phukien from "../images/phukien.png";
 import TButtonAdvise from "./TButtonAdvise";
+import phukien from "../images/phukien.png";
 import pk1 from "../images/pk1.jpg";
 import pk5 from "../images/pk5.jpg";
 import pk3 from "../images/pk3.jpg";
@@ -13,7 +13,7 @@ import pk8 from "../images/pk8.jpg";
 const ModalAccessoryMobile = () => {
 
     return (<div id="myModalAccessoryM" className="modal--thanghn">
-        <div className="modal-content--thanghn2">
+        <div className="modal-content--thanghn21">
             <div style={{display: 'flex', flex: 1, gap: 10, flexDirection: 'column', position: 'relative'}}>
                 <div style={{padding: 20, display: 'flex', flexDirection: 'row', flex: 1, backgroundColor: '#061F4D'}}>
                     <img src={phukien} style={{
@@ -23,7 +23,7 @@ const ModalAccessoryMobile = () => {
                         marginLeft: 20, display: 'flex', flexDirection: 'column', flex: 3, alignItems: 'start'
                     }}>
                         <h3 style={{color: 'white', fontWeight: 'bold'}}>Phụ kiện</h3>
-                        <p style={{color: 'white', textAlign: 'left', fontSize: 12}}>Phụ kiện là thứ không thể thiếu mỗi
+                        <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>Phụ kiện là thứ không thể thiếu mỗi
                             khi ra sân thi đấu. Ở CHF, chúng tôi có sẵn các mẫu balo, tất vớ, băng cuốn, bóng thi
                             đấu,... Ngoài ra, CHF cũng hỗ trợ việc in ấn/ thêu logo, tên, số lên các sản phẩm phụ kiện
                             như tất, balo, mũ nón....</p>
