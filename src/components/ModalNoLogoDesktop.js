1cabfbdcf40285c0b74d83789a057445ea90929f
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-26 11:36:20 +0700

    Cm

--- src/components/ModalNoLogoDesktop.js
+++ src/components/ModalNoLogoDesktop.js
@@ -18,8 +18,7 @@ const ModalNoLogoDesktop = () => {
 
     return (<div id="myModalNoLogo" className="modal--thanghn">
         <div className="modal-content--thanghn3">
-
-            <div style={{display: 'flex', flex: 1, gap: 10, flexDirection: 'column',position:'relative'}}>
+            <div style={{display: 'flex', flex: 1, gap: 10, flexDirection: 'column', position: 'relative'}}>
                 <div style={{display: 'flex', flexDirection: 'row', flex: 1, backgroundColor: '#061F4D'}}>
                     <img src={maukhonglogocosan} style={{height: 160, width: 'auto'}}/>
                     <div style={{display: 'flex', flexDirection: 'column', flex: 3, alignItems: 'start'}}>
@@ -28,60 +27,69 @@ const ModalNoLogoDesktop = () => {
                             Kamito, Bulbal</p>
                         <TButtonAdvise/>
                     </div>
-                    <span style={{paddingRight: 5, paddingLeft: 5, position:'absolute', top:0, right:10}}
+                    <span style={{paddingRight: 5, paddingLeft: 5, position: 'absolute', top: 0, right: 10}}
                           className="close" onClick={() => {
                         var modal = document.getElementById("myModalNoLogo");
                         modal.style.display = "none";
                     }}>&times;</span>
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{display: 'flex', flexDirection: 'row', gap:10}}>
-                        <div style={{display: 'flex', flex: 1}}>
-                            <img src={nologo1} style={{width: '50%', height: 'auto'}}/>
-                        </div>
-                        <div style={{display: 'flex', flex: 1}}>
-                            <img src={nologo2} style={{width: '50%', height: 'auto'}}/>
-                        </div>
-                        <div style={{display: 'flex', flex: 1}}>
-                            <img src={nologo3} style={{width: '50%', height: 'auto'}}/>
-                        </div>
-                        <div style={{display: 'flex', flex: 1}}>
-                            <img src={nologo4} style={{width: '50%', height: 'auto'}}/>
-                        </div>
+                    <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent:'center'}}>
+                        <img src={nologo1} style={{
+                            width: 190.521,
+                            height: 253.773
+                        }}/>
+                        <img src={nologo2} style={{
+                            width: 190.521,
+                            height: 253.773
+                        }}/>
+                        <img src={nologo3} style={{
+                            width: 190.521,
+                            height: 253.773
+                        }}/>
+                        <img src={nologo4} style={{
+                            width: 190.521,
+                            height: 253.773
+                        }}/>
                     </div>
                 </div>
-                {/*<div style={{display: 'flex', flexDirection: 'column'}}>*/}
-                {/*    <div style={{display: 'flex', flexDirection: 'row', gap:10}}>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo5} style={{width: '50%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo6} style={{width: '50%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo7} style={{width: '50%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo8} style={{width: '50%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                {/*<div style={{display: 'flex', flexDirection: 'column'}}>*/}
-                {/*    <div style={{display: 'flex', flexDirection: 'row', gap:10}}>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo9} style={{width: '50%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            <img src={nologo10} style={{width: '100%', height: 'auto'}}/>*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            /!*<img src={nologo7} style={{width: '100%', height: 'auto'}}/>*!/*/}
-                {/*        </div>*/}
-                {/*        <div style={{display: 'flex', flex: 1}}>*/}
-                {/*            /!*<img src={nologo8} style={{width: '100%', height: 'auto'}}/>*!/*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent:'center'}}>
+                    <img src={nologo5} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <img src={nologo6} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <img src={nologo7} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <img src={nologo8} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                </div>
+
+                <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent:'center'}}>
+                    <img src={nologo9} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <img src={nologo10} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <div style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                    <div src={nologo4} style={{
+                        width: 190.521,
+                        height: 253.773
+                    }}/>
+                </div>
             </div>
         </div>
     </div>)
