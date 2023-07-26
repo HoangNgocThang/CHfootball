1ad242bdb681e4f911b7492ccf8f54f2ceb33375
HoangNgocThang <thanghai.tb123@gmail.com>
2023-07-26 16:21:28 +0700

    cm

--- src/components/ModalAccessoryDesktop.js
+++ src/components/ModalAccessoryDesktop.js
@@ -1,28 +1,40 @@
 import React from "react";
-import maukhonglogocosan from "../images/maukhonglogocosan.png";
 import TButtonAdvise from "./TButtonAdvise";
-import nologo1 from "../images/nologo1.png";
-import nologo2 from "../images/nologo2.png";
-import nologo3 from "../images/nologo3.jpg";
-import nologo4 from "../images/nologo4.jpg";
-import nologo5 from "../images/nologo5.jpg";
-import nologo6 from "../images/nologo6.jpg";
-import nologo7 from "../images/nologo7.jpg";
-import nologo8 from "../images/nologo8.jpg";
-import nologo9 from "../images/nologo9.jpg";
-import nologo10 from "../images/nologo10.jpg";
+import phukien from "../images/phukien.png";
+import clb1 from "../images/clb1.jpg";
+import clb2 from "../images/clb2.jpg";
+import clb3 from "../images/clb3.jpg";
+import clb4 from "../images/clb4.jpg";
+import clb5 from "../images/clb5.jpg";
+import clb6 from "../images/clb6.jpg";
+import clb7 from "../images/clb7.jpg";
+
+import pk1 from '../images/pk1.jpg';
+import pk2 from '../images/pk2.jpg';
+import pk3 from '../images/pk3.jpg';
+import pk4 from '../images/pk4.jpg';
+import pk5 from '../images/pk5.jpg';
+import pk6 from '../images/pk6.jpg';
+import pk7 from '../images/pk7.jpg';
+import pk8 from '../images/pk8.jpg';
 
 const ModalAccessoryDesktop = () => {
 
     return (<div id="myModalAccessory" className="modal--thanghn">
-        <div className="modal-content--thanghn3">
+        <div className="modal-content--thanghn4">
             <div style={{display: 'flex', flex: 1, gap: 10, flexDirection: 'column', position: 'relative'}}>
-                <div style={{display: 'flex', flexDirection: 'row', flex: 1, backgroundColor: '#061F4D'}}>
-                    <img src={maukhonglogocosan} style={{height: 160, width: 'auto'}}/>
-                    <div style={{display: 'flex', flexDirection: 'column', flex: 3, alignItems: 'start'}}>
-                        <p style={{color: 'white'}}>Mẫu không logo có sẵn</p>
-                        <p style={{color: 'white', textAlign: 'left'}}>1CHFootball cung cấp các sản phẩm các thương hiệu
-                            Việt Nam như Kamito, Bulbal, Riki, CP, Wika.... với nhiều mẫu mã, chất liệu đa dạng.</p>
+                <div style={{padding: 20, display: 'flex', flexDirection: 'row', flex: 1, backgroundColor: '#061F4D'}}>
+                    <img src={phukien} style={{
+                        height: 160, width: 'auto', backgroundColor: 'white', borderRadius: 10, overflow: 'hidden'
+                    }}/>
+                    <div style={{
+                        marginLeft: 20, display: 'flex', flexDirection: 'column', flex: 3, alignItems: 'start'
+                    }}>
+                        <h3 style={{color: 'white', fontWeight: 'bold'}}>Mẫu câu lạc bộ có sẵn</h3>
+                        <p style={{color: 'white', textAlign: 'left', fontSize: 12}}>Phụ kiện là thứ không thể thiếu mỗi
+                            khi ra sân thi đấu. Ở CHF, chúng tôi có sẵn các mẫu balo, tất vớ, băng cuốn, bóng thi
+                            đấu,... Ngoài ra, CHF cũng hỗ trợ việc in ấn/ thêu logo, tên, số lên các sản phẩm phụ kiện
+                            như tất, balo, mũ nón....</p>
                         <TButtonAdvise/>
                     </div>
                     <span style={{paddingRight: 5, paddingLeft: 5, position: 'absolute', top: 0, right: 10}}
@@ -31,62 +43,23 @@ const ModalAccessoryDesktop = () => {
                         modal.style.display = "none";
                     }}>&times;</span>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent: 'center'}}>
-                        <img src={nologo1} style={{
-                            width: 190.521,
-                            height: 253.773
-                        }}/>
-                        <img src={nologo2} style={{
-                            width: 190.521,
-                            height: 253.773
-                        }}/>
-                        <img src={nologo3} style={{
-                            width: 190.521,
-                            height: 253.773
-                        }}/>
-                        <img src={nologo4} style={{
-                            width: 190.521,
-                            height: 253.773
-                        }}/>
+                <div className="container-products" style={{margin: 10}}>
+                    <div className='iteamcard'>
+                        <img src={pk1} className={"container-customer-img3"} alt={'customer'}/>
+                        <img src={pk5} className={"container-customer-img3"} alt={'customer'}/>
                     </div>
-                </div>
-                <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent: 'center'}}>
-                    <img src={nologo5} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <img src={nologo6} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <img src={nologo7} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <img src={nologo8} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                </div>
-
-                <div style={{display: 'flex', flexDirection: 'row', gap: 20, justifyContent: 'center'}}>
-                    <img src={nologo9} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <img src={nologo10} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <div style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
-                    <div src={nologo4} style={{
-                        width: 190.521,
-                        height: 253.773
-                    }}/>
+                    <div className='iteamcard'>
+                        <img src={pk3} className={"container-customer-img3"} alt={'customer'}/>
+                        <img src={pk6} className={"container-customer-img3"} alt={'customer'}/>
+                    </div>
+                    <div className='iteamcard'>
+                        <img src={pk4} className={"container-customer-img3"} alt={'customer'}/>
+                        <img src={pk7} className={"container-customer-img3"} alt={'customer'}/>
+                    </div>
+                    <div className='iteamcard'>
+                        <img src={pk2} className={"container-customer-img3"} alt={'customer'}/>
+                        <img src={pk8} className={"container-customer-img3"} alt={'customer'}/>
+                    </div>
                 </div>
             </div>
         </div>
