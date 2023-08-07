d6464dbb5dc4dce01a3530b8a0302f1b81c789fb
HoangNgocThang <thanghai.tb123@gmail.com>
2023-08-07 11:33:19 +0700

    cm

--- src/components/ModalDetailDesktop.js
+++ src/components/ModalDetailDesktop.js
@@ -1,10 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {useClickOutside} from 'use-events';
 
 const ModalDetailDesktop = () => {
 
     const [isSmall, setIsSmall] = useState(false)
     const [w, setW] = useState(window.innerWidth);
-
+    console.log('w,', w)
+    const ref1 = React.useRef(null);
+    const [isActive] = useClickOutside([ref1], event => {
+        var modal = document.getElementById("myModalDetailDesktop");
+        modal.style.display = "none";
+    });
     // function checkMediaQuery() {
     //     // If the inner width of the window is greater then 768px
     //     if (window.innerWidth <=768) {
@@ -23,36 +29,37 @@ const ModalDetailDesktop = () => {
     //     }
     // },)
 
-    console.log('w,', w)
-
-    return (<div id="myModalDetailDesktop" className="modal--thanghn">
+    return (<div id="myModalDetailDesktop" className="modal--thanghn" style={{zIndex: 10}}>
         <div className="modal-content--thanghn5" style={{position: 'relative',}}>
-            <div style={{
-                display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
-            }}>
-                <div style={{
-                    padding: 20,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center'
+            <div
+                style={{
+                    display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
                 }}>
+                <div
+                    style={{
+                        padding: 20,
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
                     <img id={'imgMyModalDetailDesktop'}
+                         ref={ref1}
                          style={w <= 390 ? {width: 350, height: 'auto'} : {height: 'auto', width: 'auto'}}/>
-                    <span style={{
-                        color: 'black',
-                        fontSize: 50,
-                        paddingRight: 5,
-                        paddingLeft: 5,
-                        position: 'absolute',
-                        top: 0,
-                        right: 10
-                    }}
-                          className="close" onClick={() => {
-                        var modal = document.getElementById("myModalDetailDesktop");
-                        modal.style.display = "none";
-                    }}>&times;</span>
+                    {/*<span style={{*/}
+                    {/*    color: 'black',*/}
+                    {/*    fontSize: 50,*/}
+                    {/*    paddingRight: 5,*/}
+                    {/*    paddingLeft: 5,*/}
+                    {/*    position: 'absolute',*/}
+                    {/*    top: 0,*/}
+                    {/*    right: 10*/}
+                    {/*}}*/}
+                    {/*      className="close" onClick={() => {*/}
+                    {/*    var modal = document.getElementById("myModalDetailDesktop");*/}
+                    {/*    modal.style.display = "none";*/}
+                    {/*}}>&times;</span>*/}
                 </div>
             </div>
         </div>
