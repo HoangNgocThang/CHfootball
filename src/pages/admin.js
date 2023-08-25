4d1a0d859a430061694333cd3725ff2603db8a31
HoangNgocThang <thanghai.tb123@gmail.com>
2023-08-25 15:05:16 +0700

    cm

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,7 +4,20 @@ function Admin() {
 
     return (
         <div>
-            Admin
+            <div style={{minHeight: 500}}>
+                <h3>Xadmin</h3>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page">Sản phẩm</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled">Khách hàng</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" tabIndex="-1" aria-disabled="true">Đối tác</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
